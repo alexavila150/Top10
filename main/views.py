ead3d93337ce8e6f6f8a88150a1ca47731a96bb0
Alex <alexadrianavila@gmail.com>
2022-04-08 16:16:15 -0600

    add list in template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from main.models import Item
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    items = Item.objects.all().order_by('-value')
+
+    context = {
+        'items' : items
+    }
+
+    return render(request, 'main/index.html', context)
